ae95431cea2c29246272ec089f220ab98058213c
Matt Amy <matt.e.amy@gmail.com>
2018-08-31 16:05:54 -0400

    Working example of using ReVerC in a Q# program

---
+++ Samples/ReVerC/AdderGen.fsx
@@ -0,0 +1,21 @@
+open System.IO
+open System
+open ReVerC
+
+let carryRippleAdder n =
+    <@
+    fun (a : bool array) (b : bool array) ->
+      let compute_carry (a : bool) (b : bool) (c : bool) =
+        (a && (b <> c)) <> (b && c) // a && b <> a && c <> b && c
+
+      let result = Array.zeroCreate(n)
+      let mutable carry = false
+      result.[0] <- a.[0] <> b.[0]
+      for i in 1 .. n-1 do
+        carry <- compute_carry a.[i-1] b.[i-1] carry
+        result.[i]  <-  a.[i] <> b.[i] <> carry
+      result           
+    @>
+
+File.WriteAllText("adder.qs", printQSharp "adder" <| compile (carryRippleAdder 2) false Eager)
+0
